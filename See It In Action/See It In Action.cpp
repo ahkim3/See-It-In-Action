4f2086ba8fc575f7b0b68bef2f907b889dce3e8e
Andrew Kim <66102642+ahkim3@users.noreply.github.com>
2021-03-15 00:31:14 -0500

    Add project files.

---
+++ See It In Action/See It In Action.cpp
@@ -0,0 +1,40 @@
+/*
+Name: Andrew Kim
+Date: March 15, 2021
+Program: Rational
+
+I hereby certify that this program represents my
+work and that the design and logic was completed
+without outside assistance.
+
+Signature: Andrew Kim
+*/
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(600, 600), "See It In Action");
+    sf::RectangleShape interior(sf::Vector2f(500, 500));
+    interior.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(interior);
+        window.display();
+    }
+
+    return 0;
+}
